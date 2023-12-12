9a4986aa4b769aab43ef1b9e19905a2e09b95da1
melikaqorbani <melikaghorbani82@gmail.com>
2023-12-12 15:44:56 +0330

    third commit

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,6 +1,16 @@
-export default function handler(req, res){
-    const {method} = req;
-    if (method ==='POST'){
+import { Product } from "@/lib/models/product";
+import clientPromise from "@/lib/mongodb";
+import { mongooseConnect } from "@/lib/mongoose";
+import mongoose from "mongoose";
 
+export default async function handler(req, res){
+    const {method} = req;
+    await mongooseConnect;
+    if (method ==='POST'){
+        const {title,description,price} = req.body;
+        const productDoc = await Product.create({
+            title,description,price,
+        })
+        res.json(productDoc);
     }
 }
